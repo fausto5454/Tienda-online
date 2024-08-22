a13001e1976d50e22a8d683e58ec70f3d68505b4
332590 <dvasquezespinoza1@gmail.com>
2024-08-21 19:30:28 -0500

    mi tarea

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto">
+        <p className="text-white text-center">
+          Copyright &copy; Productos Shopping 2024. Todos los derechos reservados.
+        </p>
+      </div>
+    </footer>
+  );
+};
